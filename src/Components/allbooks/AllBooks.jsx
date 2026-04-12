80e24b9fd24416ebc3c0fa407b2513070ee9a088
NJ-NafsinJahan <nj.nafsinjahan@gmail.com>
2026-04-12 15:38:42 +0600

    BookCard added

--- src/Components/allbooks/AllBooks.jsx
+++ src/Components/allbooks/AllBooks.jsx
@@ -1,4 +1,5 @@
 import React, { use } from "react";
+import BookCard from "../ui/BookCard";
 
 const booksPromise = fetch("/booksData.json").then((res) => res.json());
 
@@ -6,10 +7,15 @@ const AllBooks = () => {
   const books = use(booksPromise);
   console.log(books, "books");
   return (
-    <div className="py-13">
-      <h1 className=" text-2xl text-amber-600 font-extrabold text-center">
-        AllBooks
+    <div className="py-13 container mx-auto ">
+      <h1 className=" text-4xl text-[#131313]/75 font-bold text-center mb-6">
+        Books
       </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        {books.map((book) => {
+          return <BookCard key={book.bookId} book={book} />;
+        })}
+      </div>
     </div>
   );
 };
---
+++ src/Components/ui/BookCard.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { FaRegStar } from "react-icons/fa";
+
+const BookCard = ({ book }) => {
+  return (
+    <div className="card bg-base-100 shadow-sm border border-gray-100">
+      <div className=" p-5  ">
+        <figure className="p-6 bg-base-300 rounded-2xl">
+          <img className="rounded-xl h-55" src={book.image} alt="" />
+        </figure>
+      </div>
+      <div className="card-body">
+        <div className="flex items-center gap-4">
+          {book.tags.map((tag, ind) => (
+            <div
+              key={ind}
+              className="badge badge-soft badge-success text-[#23BE0A] font-medium text-[18px] "
+            >
+              {tag}
+            </div>
+          ))}
+        </div>
+
+        <h2 className="font-bold text-[24px] text-[#131313]/70">
+          {" "}
+          {book.bookName}
+        </h2>
+
+        <p className="font-normal text-[16px] text-[#131313]/70">
+          {book.author}
+        </p>
+
+        <div className="card-actions justify-between border-t border-dashed border-gray-400 pt-4">
+          <div className="font-normal text-[18px] text-[#131313]/70 ">
+            {book.category}
+          </div>
+          <div className="font-normal text-[18px] text-[#131313]/70 flex items-center gap-3">
+            {book.rating} <FaRegStar />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookCard;
